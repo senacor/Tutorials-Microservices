7371ad7f7bf1cc2bcc9062daba94958ccfb48180
Daniel Karzel <daniel.karzel@senacor.com>
2017-07-27 19:08:04 +0200

    flyway configuration and 3 migration scripts

---
+++ demo/src/main/resources/db/migration/V1__Create_customer_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE demodb.customer (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL
+);
---
+++ demo/src/main/resources/db/migration/V3__Alter_customer_table_AddColumnComment.sql
@@ -0,0 +1,1 @@
+ALTER TABLE demodb.customer ADD COLUMN comment VARCHAR(100) DEFAULT 'cool guy';
