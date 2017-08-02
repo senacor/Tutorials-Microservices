63cecc4fafc36681543b3cf20df0d1b3e697926f
Daniel Karzel <daniel.karzel@senacor.com>
2017-08-02 13:38:01 +0200

    added registry service, feign client not configured yet.

--- demo/src/main/java/com/senacor/bitc/demo/DemoApplication.java
+++ demo/src/main/java/com/senacor/bitc/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.senacor.bitc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
