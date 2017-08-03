5c4586acc27f84f6e865ab5c45b645d3350a6a3a
Daniel Karzel <daniel.karzel@senacor.com>
2017-08-03 12:50:41 +0200

    add @Configuration to accounting application to make the feign client work. note that the first request still sometimes runs into a timeout. extending the timeout dit not help

--- accounting/src/main/java/com/senacor/bitc/demo/AccountingApplication.java
+++ accounting/src/main/java/com/senacor/bitc/demo/AccountingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@Configuration
 public class AccountingApplication {
 
 	public static void main(String[] args) {
